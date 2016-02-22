240fd4142fcc65235bf78038f539b3966f89af82
Andrea Zonca <code@andreazonca.com>
2016-02-22 09:43:11 -0800

    module

--- python/launch_notebook.sh
+++ python/launch_notebook.sh
@@ -3,4 +3,5 @@ echo "**************************************************************************
 echo "After IPython notebook starts up, connect with your laptop browser to `hostname`:8888"
 echo "************************************************************************************"
 
+module load python scipy
 ipython notebook --no-browser --ip=*
